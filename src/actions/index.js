a9229eab0cb142a4f5648de087dd97555c06a0c3
Peter Daily <thepeted@gmail.com>
2016-04-02 14:05:05 +0100

    linting

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,37 +3,37 @@ export function toggleAlive(x,y) {
     type: 'TOGGLE_ALIVE',
     x,
     y
-  }
-};
+  };
+}
 
 export function makeRandomGrid() {
   return {
     type: 'MAKE_RANDOM'
-  }
-};
+  };
+}
 
 export function tick() {
   return {
     type: 'TICK'
-  }
+  };
 }
 
 export function startPlaying(timerId) {
   return {
     type: 'PLAY',
     timerId
-  }
+  };
 }
 
 export function stopPlaying(timerId) {
   return {
     type: 'STOP',
     timerId
-  }
+  };
 }
 
 export function clear() {
   return {
     type: 'CLEAR',
-  }
+  };
 }
