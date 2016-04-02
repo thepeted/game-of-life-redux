39f55d3dcb67d61f86e4c282f00504fc75ff97e0
Peter Daily <thepeted@gmail.com>
2016-04-02 14:26:38 +0100

    convert app to a functional component

--- src/components/app.js
+++ src/components/app.js
@@ -5,23 +5,19 @@ import Counter from '../containers/counter';
 import GitHubForkRibbon from 'react-github-fork-ribbon';
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Game of Life (React-Redux)</h1>
-        <Board />
-        <Control />
-        <Counter />
-        <GitHubForkRibbon
-          href='https://github.com/thepeted/game-of-life-redux'
-          target='_blank'
-          position='right'
-          color='orange'
-          >
-          Fork me on GitHub
-        </GitHubForkRibbon>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <h1>Game of Life (React-Redux)</h1>
+    <Board />
+    <Control />
+    <Counter />
+    <GitHubForkRibbon
+      href='https://github.com/thepeted/game-of-life-redux'
+      target='_blank'
+      position='right'
+      color='orange'
+      >
+      Fork me on GitHub
+    </GitHubForkRibbon>
+  </div>
+)
