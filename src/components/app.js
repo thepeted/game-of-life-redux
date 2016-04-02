982cf4624f261b3218fbfe1e41d8f6f573d779d2
Peter Daily <thepeted@gmail.com>
2016-04-02 12:52:32 +0100

    add fork me ribbon

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Board from '../containers/board';
 import Control from '../containers/control';
 import Counter from '../containers/counter';
+import GitHubForkRibbon from 'react-github-fork-ribbon';
 
 
 export default class App extends Component {
@@ -12,6 +13,14 @@ export default class App extends Component {
         <Board />
         <Control />
         <Counter />
+        <GitHubForkRibbon
+          href='https://github.com/thepeted/game-of-life-redux'
+          target='_blank'
+          position='right'
+          color='orange'
+          >
+          Fork me on GitHub
+        </GitHubForkRibbon>
       </div>
     );
   }
